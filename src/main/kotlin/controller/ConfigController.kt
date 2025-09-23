40fba820ac02c3e9134cbb047394596ff21dbc85
Marcos <844419@unizar.es>
2025-09-23 19:39:38 +0200

    dynamic contex translated

--- src/main/kotlin/controller/ConfigController.kt
+++ src/main/kotlin/controller/ConfigController.kt
@@ -1,10 +1,7 @@
 package es.unizar.webeng.hello.controller
 
-import org.aopalliance.intercept.Interceptor
-import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -14,7 +11,7 @@ import java.util.Locale
 
 @Configuration
 class ConfigController : WebMvcConfigurer {
-    
+
 
     @Bean
     fun localeResolver() : LocaleResolver {
